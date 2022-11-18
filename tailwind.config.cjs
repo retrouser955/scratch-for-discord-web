c5771efa912461f9b7a4c794106fb38584dd41fb
Retro <htoomyataung382@gmail.com>
2022-11-18 19:00:20 +0630

    extended theme with green 900

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          900: "#3BA55D"
+        }
+      }
+    },
   },
   plugins: [],
   darkMode: "class"
