f696b80169bd8f7716b4a4f7ab1d737b433c655c
Retro <htoomyataung382@gmail.com>
2023-02-08 12:49:40 +0630

    update kinda

--- src/components/File-Selector/FileSelector.jsx
+++ src/components/File-Selector/FileSelector.jsx
@@ -1,18 +1,25 @@
 import "./selector.css"
 import { useState, useEffect } from "react"
 import switchFiles from "../../utils/switchFiles"
+import { AiFillCloseCircle } from "react-icons/ai"
 
 export default function FileSelector({ tabs }) {
     const [tabEle, setTabEle] = useState([])
 
     useEffect(() => {
-        const reactTabs = tabs.map((value) => {
+        const reactTabs = tabs.map((value, i) => {
             return <>
-                <div className="items my-auto" onClick={() => {
+                <div className="items border-r-2 border-stone-900 bg-stone-700" onClick={() => {
                     switchFiles(value)
                 }}>
-                    <div className="h-full w-full flex">
+                    <div className="h-full overflow-hidden items-center flex w-[85%]">
                         {value}.js
+                    </div>
+                    <div className="w-[15%] flex justify-center items-center h-full">
+                        <AiFillCloseCircle onClick={() => {
+                            tabEle.splice(i, 1)
+                            setTabEle(tabEle)
+                        }} />
                     </div>
                 </div>
             </>
@@ -22,7 +29,7 @@ export default function FileSelector({ tabs }) {
     }, [])
 
     return (
-        <div className="w-full h-[5%] overflow-x-auto bg-stone-900 flex">
+        <div className="w-full h-[5%] border-b-[1px] overflow-x-auto bg-stone-900 flex">
             {tabEle}
         </div>
     )
